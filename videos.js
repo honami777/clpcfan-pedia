c749d78e92eb92a14bc5584bcbf1f8203b4df60c
保奈美 <honami77@yahoo.co.jp>
2026-06-04 00:58:38 +0900

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,16 @@
+const videos = [
+  {
+    title: "サンプル動画1",
+    members: ["じゃぱぱ", "のあ"],
+    series: "マイクラ",
+    collab: "",
+    url: "https://www.youtube.com/"
+  },
+  {
+    title: "ドズル社コラボ サンプル",
+    members: ["じゃぱぱ", "たっつん"],
+    series: "コラボ",
+    collab: "ドズル社",
+    url: "https://www.youtube.com/"
+  }
+];
